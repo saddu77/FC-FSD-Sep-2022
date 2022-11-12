6efe50cd10efbd3bcfc13eee8afcd8eca1d363ad
User <sadakhath07@gmail.com>
2022-11-12 16:44:20 +0530

    my twenth commit

---
+++ DOM-manipulations/validate.js
@@ -0,0 +1,53 @@
+
+
+function showMessage(input,msg,type){
+    const message = input.parentNode.querySelector("small"); 
+   // alert(message)
+   // message.innerText = msg;
+    input.className = type? "success" : "error";
+    return type;
+}
+
+function showError(input,msg){
+    return showMessage(input,msg,false);
+}
+
+function showSuccess(input){
+    return showMessage(input,"",true);
+}
+
+function hasValue(input,msg){
+    if(input.value.trim() === ""){
+        return showError(input,msg);
+    }
+    return showSuccess(input);
+}
+
+function validateEmail(input,requiredMsg,invalidMsg){
+    if(!hasValue(input,requiredMsg)){
+        return false;
+    }
+
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const email = input.value.trim();
+    if(!emailRegex.test(email)){
+        return showError(input,invalidMsg);
+    }
+    return true;
+}
+
+const form = document.querySelector("#signup");
+
+const name_required = "Enter Your Name";
+const email_required = "Enter your Email";
+const email_invalid = "Enter Correct Email";
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    let nameValid = hasValue(form.elements["firstName"],name_required);
+    let emailValid = validateEmail(form.elements["emailAddress"],email_required,email_invalid);
+
+    if(nameValid && emailValid){
+        alert("Data Saved Success");
+    }
+})
